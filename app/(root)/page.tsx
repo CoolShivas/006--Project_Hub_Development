4f2401a58548a9d6d3c5ed954c09690539d3b6c9
CoolShivas <shivas2710cool00@gmail.com>
2025-08-31 00:12:44 +0530

    Creation of Trending Projects section to show the card lists.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,7 +5,7 @@ import SearchForm from "@/components/SearchForm";
 const page = () => {
   return (
     <>
-      <div className="h-[730px] relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg">
+      <div className="h-[530px] relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg">
         <div className="absolute inset-0 w-full h-full  z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
 
         <Boxes />
@@ -15,6 +15,35 @@ const page = () => {
         </p>
         <SearchForm></SearchForm>
       </div>
+      <section className="section_container">
+        <p className="text-30-semibold">Trending Projects</p>
+        <ul className="mt-7 card_grid grid grid-cols-1 sm:grid-cols-1 gap-1 justify-center">
+          {[
+            {
+              id: 123,
+              title: "A new project",
+              description: "It is the best project.",
+            },
+            {
+              id: 123,
+              title: "A new project",
+              description: "It is the best project.",
+            },
+            {
+              id: 123,
+              title: "A new project",
+              description: "It is the best project.",
+            },
+            {
+              id: 123,
+              title: "A new project",
+              description: "It is the best project.",
+            },
+          ].map((items, index) => {
+            return <div>{items.title}</div>;
+          })}
+        </ul>
+      </section>
     </>
   );
 };
