ce7cdeaf74e01c0b27231a6237e200c65cc29c64
CoolShivas <shivas2710cool00@gmail.com>
2025-08-31 14:26:24 +0530

    Some CSS correction in dynamic routing user page.

--- app/(root)/users/[id]/page.tsx
+++ app/(root)/users/[id]/page.tsx
@@ -6,7 +6,7 @@ const page = ({ params }: { params: { id: string } }) => {
   return (
     <>
       <section className="profile_container">
-        <div className="border border-black/[0.2] dark:border-white/[0.2] flex flex-col items-start max-w-sm mx-auto p-4 relative h-[30rem]">
+        <div className="border border-black/[0.2] dark:border-white/[0.2] flex flex-col  max-w-sm mx-auto p-4 relative h-[35rem] items-center">
           <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
           <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
           <Icon className="absolute h-6 w-6 -top-3 -right-3 dark:text-white text-black" />
@@ -14,12 +14,12 @@ const page = ({ params }: { params: { id: string } }) => {
 
           <EvervaultCard text="hover" />
 
-          <h2 className="dark:text-white text-black mt-4 text-sm font-light">
-            Hover over this card to reveal an awesome effect. Running out of
-            copy here.
+          <h2 className="dark:text-white text-black mt-4 text-lg font-bold text-center">
+            Name and Last-Name
           </h2>
-          <p className="text-sm border font-light dark:border-white/[0.2] border-black/[0.2] rounded-full mt-4 text-black dark:text-white px-2 py-0.5">
-            Watch me hover
+          <p className="text-sm border font-light dark:border-white/[0.2] border-black/[0.2] rounded-full mt-4 text-black dark:text-white px-2 py-0.5 pb-5 text-center">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati,
+            autem?
           </p>
         </div>
       </section>
