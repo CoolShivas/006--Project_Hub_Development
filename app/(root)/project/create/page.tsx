9b748ca56bdb58065a699ded5c3b5ff635b148ad
CoolShivas <shivas2710cool00@gmail.com>
2025-08-31 18:59:29 +0530

    Formation of new create page.

---
+++ app/(root)/project/create/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CreatePage = () => {
+  return (
+    <>
+      <section className="form_container !min-h-[230px]">
+        <h1 className="heading">Submit Your Project</h1>
+      </section>
+    </>
+  );
+};
+
+export default CreatePage;
