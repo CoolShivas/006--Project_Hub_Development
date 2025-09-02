7a9bbf30ee99a57ba9bd7f3912590163569d55ed
CoolShivas <shivas2710cool00@gmail.com>
2025-09-02 12:45:08 +0530

    Now, using the nullish operator for conditional rendering whether user login or logout.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { auth } from "@/auth";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await auth();
+
   return (
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
       <nav className="flex justify-between items-center">
@@ -10,17 +13,24 @@ const Navbar = () => {
           <Image src="/LogoShiva.jpg" alt="logo" width={100} height={2} />
         </Link>
         <div className="flex items-center gap-5 text-black">
-          <Link href="/">
-            <span className="text-6 font-sans max-sm:hidden">Create</span>
-          </Link>
+          {session && session?.user ? (
+            <>
+              {/* If user is already LogIn then it he/she is able to see this pages */}
+              <Link href="/">
+                <span className="text-6 font-sans max-sm:hidden">Create</span>
+              </Link>
 
-          <Link href="/">
-            <span className="text-6 font-sans max-sm:hidden">Logout</span>
-          </Link>
+              <Link href="/">
+                <span className="text-6 font-sans max-sm:hidden">Logout</span>
+              </Link>
 
-          <Link href="/">
-            <span className="text-6 font-sans max-sm:hidden">Username</span>
-          </Link>
+              <Link href="/">
+                <span className="text-6 font-sans max-sm:hidden">Username</span>
+              </Link>
+            </>
+          ) : (
+            <>{/* Else first SignUp to move instead */}</>
+          )}
         </div>
       </nav>
     </header>
