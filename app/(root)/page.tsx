e8fc1abe993f3b93810031d3b6c56bff2241bd87
CoolShivas <shivas2710cool00@gmail.com>
2025-09-03 21:17:02 +0530

    Now, connecting the backend to frontend. And, formation of SearchFormReset and ProjectCard. And changes in (root) page and searchForm also.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,8 +2,24 @@ import React from "react";
 import { Boxes } from "@/components/ui/background-boxes";
 import SearchForm from "@/components/SearchForm";
 import { ThreeDCardDemo } from "../../components/ThreeDCard";
+import { auth } from "@/auth";
+import { sanityFetch } from "@/sanity/lib/live";
+import { PROJECT_QUERY } from "@/sanity/lib/queries";
+import { ProjectTypeCard } from "@/components/ProjectCard";
 
-const page = () => {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+  const params = { search: query || null };
+  const session = await auth();
+  // console.log(session?.id);
+
+  const { data: posts } = await sanityFetch({ query: PROJECT_QUERY, params });
+  console.log(posts);
+
   return (
     <>
       <div className="h-[530px] relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg">
@@ -14,12 +30,12 @@ const page = () => {
         <p className="sub-heading !max-w-3xl relative">
           where technology meets with your idea
         </p>
-        <SearchForm></SearchForm>
+        <SearchForm query={query}></SearchForm>
       </div>
       <section className="section_container">
         <p className="text-30-semibold">Trending Projects</p>
         <ul className="mt-5 card_grid grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-3 gap-1 justify-center">
-          {[
+          {/* {[
             {
               id: 123,
               title: "A new project",
@@ -42,11 +58,16 @@ const page = () => {
             },
           ].map((items, index) => {
             return <ThreeDCardDemo key={index} />;
-          })}
+          })} */}
+          {posts?.length > 0 ? (
+            posts.map((post: ProjectTypeCard, index: number) => {
+              return <ThreeDCardDemo key={post._id} />;
+            })
+          ) : (
+            <p className="no-results">No Proojects Found</p>
+          )}
         </ul>
       </section>
     </>
   );
-};
-
-export default page;
+}
