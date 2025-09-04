0c656f0e68e60ed4f05b8e3377cbf9c2d21b8753
CoolShivas <shivas2710cool00@gmail.com>
2025-09-05 01:14:39 +0530

    Redirecting on card image or detail click to that page. And, use of destructuring the properities from post.

--- components/ThreeDCard.tsx
+++ components/ThreeDCard.tsx
@@ -3,8 +3,20 @@
 import React from "react";
 import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
 import { ProjectTypeCard } from "./ProjectCard";
+import Link from "next/link";
 
 export function ThreeDCardDemo({ post }: { post: ProjectTypeCard }) {
+  const {
+    _createdAt,
+    _id,
+    title,
+    description,
+    author,
+    image,
+    views,
+    category,
+  } = post;
+
   return (
     <CardContainer className="inter-var h-[20rem]">
       <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[25rem] h-[25rem] rounded-xl p-6 border  ">
@@ -13,7 +25,7 @@ export function ThreeDCardDemo({ post }: { post: ProjectTypeCard }) {
           className="text-xl font-bold text-neutral-600 dark:text-white"
         >
           {/* Make things float in air */}
-          {post.title}
+          {title}
         </CardItem>
         <CardItem
           as="p"
@@ -21,17 +33,19 @@ export function ThreeDCardDemo({ post }: { post: ProjectTypeCard }) {
           className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
         >
           {/* Hover over this card to unleash the power of CSS perspective */}
-          {post.description}
+          {description}
         </CardItem>
         <CardItem translateZ="100" className="w-full mt-4">
-          <img
-            // src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            src={post.image}
-            height="1000"
-            width="1000"
-            className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
-            alt="thumbnail"
-          />
+          <Link href={`/project/${_id}`}>
+            <img
+              // src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              src={image}
+              height="1000"
+              width="1000"
+              className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+              alt="thumbnail"
+            />
+          </Link>
         </CardItem>
         <div className="flex justify-between items-center mt-20">
           <CardItem
@@ -42,16 +56,18 @@ export function ThreeDCardDemo({ post }: { post: ProjectTypeCard }) {
             className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
           >
             {/* Try now → */}
-            {post.author?.name}
-            <br />@{post.author?.username}
+            {author?.name}
+            <br />@{author?.username}
           </CardItem>
-          <CardItem
-            translateZ={20}
-            as="button"
-            className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
-          >
-            Sign up
-          </CardItem>
+          <Link href={`/project/${_id}`}>
+            <CardItem
+              translateZ={20}
+              as="button"
+              className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+            >
+              Details
+            </CardItem>
+          </Link>
         </div>
       </CardBody>
     </CardContainer>
